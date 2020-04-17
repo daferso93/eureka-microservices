445def00830fb55a045f9c778cfa7d256a8e4f87
daniel.fernandez <daniel.fernandezsolano@Fiserv.com>
2020-04-17 14:55:24 -0600

    Test

---
+++ client1/src/main/java/com/microservices/client1/Client1Application.java
@@ -0,0 +1,33 @@
+package com.microservices.client1;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class Client1Application implements GreetingController{
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Client1Application.class, args);
+	}
+
+	@Override
+	public String greeting() {
+		return String.format(
+				"Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+	}
+}
---
+++ client2/src/main/java/com/microservices/client2/Client2Application.java
@@ -0,0 +1,27 @@
+package com.microservices.client2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableFeignClients
+@Controller
+public class Client2Application {
+	@Autowired
+	private GreetingClient greetingClient;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Client2Application.class, args);
+	}
+
+	@RequestMapping("/get-greeting")
+	public String greeting(Model model) {
+		model.addAttribute("greeting", greetingClient.greeting());
+		return "greeting-view";
+	}
+}
